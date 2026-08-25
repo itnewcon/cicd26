ff835e2de913810a09e0ba28772b764dbb3b5d83
Vishal Teli <kcc.vishal@gmail.com>
2026-08-25 08:47:48 +0530

    Initial ETL Code

---
+++ etl.py
@@ -0,0 +1,16 @@
+def extract_data():
+    print("Extracting data")
+
+
+def transform_data():
+    print("Transforming data")
+
+
+def load_data():
+    print("Loading data")
+
+
+if __name__ == "__main__":
+    extract_data()
+    transform_data()
+    load_data()
