ca94d726c9c9f3a698775a883386edcef9d2505f
Vishal Teli <kcc.vishal@gmail.com>
2026-09-03 08:37:29 +0530

    Update-transform

--- etl.py
+++ etl.py
@@ -10,6 +10,10 @@ def load_data():
     print("Loading data")
 
 
+def transform(data):
+    print("Transforming data")
+    return [value * 2 for value in data]
+    
 if __name__ == "__main__":
     extract_data()
     transform_data()
